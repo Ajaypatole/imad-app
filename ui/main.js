0dd892826aa77b3514f5009e1acd86833181ca72
Ajaypatole <ajaypatole7623@gmail.com>
2017-08-20 00:04:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML='New Value';
 
 //move the Image
 var img = document.getElementById('madi');
-
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + '1';
     img.style.marginLeft=marginLeft + 'px';
